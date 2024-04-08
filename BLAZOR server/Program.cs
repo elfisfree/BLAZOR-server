2ff1eb350722afbfaff315863d4c14983a0ec1fc
212222 <veraharis0@gmail.com>
2024-04-08 16:12:09 +0300

    Добавлен ассинрон

--- BLAZOR server/Program.cs
+++ BLAZOR server/Program.cs
@@ -2,72 +2,103 @@
 {
     static async Task Main()
     {
-        Console.WriteLine("Выберите режим:\n1. Калькулятор\n2. Список дел");
-        int mode = Convert.ToInt32(Console.ReadLine());
-        if (mode == 1)
-        {
-            Console.WriteLine("Введите первое число:");
-            double num1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Введите второе число:");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Выберите операцию (+, -, *, /):");
-            char operation = Convert.ToChar(Console.ReadLine());
-            double result = 0;
-            switch (operation)
-            {
-                case '+':
-                    result = num1 + num2;
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                case '/':
-                    if (num2 != 0)
-                    {
-                        result = num1 / num2;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Деление на ноль недопустимо");
-                        return;
-                    }
-                    break;
-                default:
-                    Console.WriteLine("Неправильная операция");
-                    return;
-            }
-            Console.WriteLine("Результат операции: " + result);
-        }
-        else if (mode == 2)
-        {
-            List<string> toDoList = new List<string>();
-            await FillToDoListAsync(toDoList);
-            Console.WriteLine("Список дел:");
-            foreach (var task in toDoList)
-            {
-                Console.WriteLine(task);
-            }
-        }
-        else
-        {
-            Console.WriteLine("Неправильный режим, попробуйте снова.");
-        }
-    }
-    static async Task FillToDoListAsync(List<string> toDoList)
-    {
         while (true)
         {
-            Console.WriteLine("Введите новое дело: (или введите 'exit' для выхода)");
+            Console.WriteLine("~~~~~ Выберите режим ~~~~~:\n1. Калькулятор\n2. Список дел\n3. Выход");
+            int mode = Convert.ToInt32(Console.ReadLine());
+
+            if (mode == 1)
+            {
+                Console.WriteLine("Введите первое число:");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Введите второе число:");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Выберите операцию (+, -, *, /):");
+                char operation = Convert.ToChar(Console.ReadLine());
+
+                double result = 0;
+
+                switch (operation)
+                {
+                    case '+':
+                        result = num1 + num2;
+                        break;
+                    case '-':
+                        result = num1 - num2;
+                        break;
+                    case '*':
+                        result = num1 * num2;
+                        break;
+                    case '/':
+                        if (num2 != 0)
+                        {
+                            result = num1 / num2;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Деление на ноль недопустимо");
+                            return;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Неправильная операция");
+                        return;
+                }
+
+                Console.WriteLine("Результат операции: " + result);
+            }
+            else if (mode == 2)
+            {
+                List<string> toDoList = new List<string>();
+
+                await FillToDoListAsync(toDoList);
+
+                Console.WriteLine("~~~~ Список дел: ~~~~");
+                foreach (var task in toDoList)
+                {
+                    Console.WriteLine(task);
+                }
+            }
+            else if (mode == 3)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Неправильный режим, попробуйте снова.");
+            }
+        }
+    }
+
+    static async Task FillToDoListAsync(List<string> toDoList)
+    {
+        Console.WriteLine("Заполнение списка дел. Введите новое дело (или 'exit' для выхода):");
+
+        while (true)
+        {
+            Console.WriteLine("Введите новое дело ('exit' - для выхода; 'list' - для вывода списка дел):");
             string newTask = Console.ReadLine();
+
             if (newTask.ToLower() == "exit")
             {
                 break;
             }
-            toDoList.Add(newTask);
-            Console.WriteLine("Дело успешно добавлено в список!");
+            else if (newTask.ToLower() == "list")
+            {
+                Console.WriteLine(" ~~~~Список дел:~~~~ ");
+                foreach (var task in toDoList)
+                {
+                    Console.WriteLine(task);
+                }
+            }
+            else
+            {
+                toDoList.Add(newTask);
+                Console.WriteLine("Дело успешно добавлено в список!");
+            }
         }
+        await Task.CompletedTask;
     }
 }
